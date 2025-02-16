a6cace4b9c34d896b1b01cda92d64caff9fcd08c
E.S. Rosenberg a.k.a. Keeper of the Keys <es.rosenberg+github@gmail.com>
2025-02-16 10:29:46 +0200

    Add updated schema
    Signed-off-by: E.S. Rosenberg a.k.a. Keeper of the Keys <es.rosenberg+github@gmail.com>

---
+++ roles/mailserver/files/mailserver.schema.20250216.sql
@@ -0,0 +1,285 @@
+-- MariaDB dump 10.19  Distrib 10.11.6-MariaDB, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: mailserver
+-- ------------------------------------------------------
+-- Server version	10.11.6-MariaDB-0+deb12u1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `system_info`
+--
+
+DROP TABLE IF EXISTS `system_info`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `system_info` (
+  `key` varchar(50) NOT NULL,
+  `value` varchar(100) NOT NULL,
+  PRIMARY KEY (`key`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `system_info`
+--
+
+LOCK TABLES `system_info` WRITE;
+/*!40000 ALTER TABLE `system_info` DISABLE KEYS */;
+INSERT INTO `system_info` VALUES
+('schema_version','20250216');
+/*!40000 ALTER TABLE `system_info` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `virtual_aliases`
+--
+
+DROP TABLE IF EXISTS `virtual_aliases`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `virtual_aliases` (
+  `alias_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `domain_id` int(10) unsigned NOT NULL,
+  `source` varchar(100) NOT NULL,
+  `destination` varchar(100) NOT NULL,
+  `requires_identifier` tinyint(1) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`alias_id`),
+  UNIQUE KEY `domain_id_2` (`domain_id`,`source`,`destination`),
+  KEY `domain_id` (`domain_id`),
+  KEY `source` (`source`),
+  CONSTRAINT `virtual_aliases_ibfk_1` FOREIGN KEY (`domain_id`) REFERENCES `virtual_domains` (`domain_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `virtual_aliases`
+--
+
+LOCK TABLES `virtual_aliases` WRITE;
+/*!40000 ALTER TABLE `virtual_aliases` DISABLE KEYS */;
+/*!40000 ALTER TABLE `virtual_aliases` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `virtual_application_password_log`
+--
+
+DROP TABLE IF EXISTS `virtual_application_password_log`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `virtual_application_password_log` (
+  `user_id` int(10) unsigned NOT NULL,
+  `password_id` int(10) unsigned NOT NULL,
+  `access_time` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `source` varchar(100) DEFAULT NULL,
+  KEY `user_id` (`user_id`),
+  KEY `password_id` (`password_id`),
+  CONSTRAINT `virtual_application_password_log_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `virtual_users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `virtual_application_password_log_ibfk_2` FOREIGN KEY (`password_id`) REFERENCES `virtual_application_passwords` (`password_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `virtual_application_password_log`
+--
+
+LOCK TABLES `virtual_application_password_log` WRITE;
+/*!40000 ALTER TABLE `virtual_application_password_log` DISABLE KEYS */;
+/*!40000 ALTER TABLE `virtual_application_password_log` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `virtual_application_passwords`
+--
+
+DROP TABLE IF EXISTS `virtual_application_passwords`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `virtual_application_passwords` (
+  `password_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(10) unsigned NOT NULL,
+  `application_username` varchar(100) NOT NULL,
+  `application_password` varchar(106) NOT NULL,
+  `password_changed` timestamp NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`password_id`),
+  UNIQUE KEY `application_username` (`application_username`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `virtual_application_passwords_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `virtual_users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `virtual_application_passwords`
+--
+
+LOCK TABLES `virtual_application_passwords` WRITE;
+/*!40000 ALTER TABLE `virtual_application_passwords` DISABLE KEYS */;
+INSERT INTO `virtual_application_passwords` VALUES
+(0,0,'Dummy Password for Primary Password Logging','','2023-02-27 22:27:08');
+/*!40000 ALTER TABLE `virtual_application_passwords` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `virtual_domains`
+--
+
+DROP TABLE IF EXISTS `virtual_domains`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `virtual_domains` (
+  `domain_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `domain` varchar(100) NOT NULL,
+  `backend_type` enum('MySQL','LDAP') NOT NULL,
+  `backend_uri` varchar(100) DEFAULT NULL,
+  `ldap_bind_dn` varchar(100) DEFAULT NULL,
+  `ldap_bind_password` varchar(128) DEFAULT NULL,
+  `ldap_base_dn` varchar(100) DEFAULT NULL,
+  `ldap_sync_cron` tinyint(1) NOT NULL DEFAULT 0,
+  `allow_primary_password` tinyint(1) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`domain_id`),
+  UNIQUE KEY `name` (`domain`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `virtual_domains`
+--
+
+LOCK TABLES `virtual_domains` WRITE;
+/*!40000 ALTER TABLE `virtual_domains` DISABLE KEYS */;
+INSERT INTO `virtual_domains` VALUES
+(0,'Dummy Domain for Primary Password logging','MySQL',NULL,NULL,NULL,NULL,0,0);
+/*!40000 ALTER TABLE `virtual_domains` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `virtual_senders`
+--
+
+DROP TABLE IF EXISTS `virtual_senders`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `virtual_senders` (
+  `sender_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `domain_id` int(10) unsigned NOT NULL,
+  `address` varchar(100) NOT NULL,
+  `allowed_sender` varchar(100) NOT NULL,
+  PRIMARY KEY (`sender_id`),
+  UNIQUE KEY `domain_id_2` (`domain_id`,`address`,`allowed_sender`),
+  KEY `domain_id` (`domain_id`),
+  CONSTRAINT `virtual_senders_ibfk_1` FOREIGN KEY (`domain_id`) REFERENCES `virtual_domains` (`domain_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `virtual_senders`
+--
+
+LOCK TABLES `virtual_senders` WRITE;
+/*!40000 ALTER TABLE `virtual_senders` DISABLE KEYS */;
+/*!40000 ALTER TABLE `virtual_senders` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `virtual_users`
+--
+
+DROP TABLE IF EXISTS `virtual_users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `virtual_users` (
+  `user_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `domain_id` int(10) unsigned NOT NULL,
+  `primary_password` varchar(106) DEFAULT NULL,
+  `email` varchar(100) NOT NULL,
+  `active` tinyint(1) DEFAULT 1,
+  `system_user` tinyint(1) NOT NULL DEFAULT 0,
+  `secondary_mail` varchar(100) DEFAULT NULL,
+  `password_changed` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `password_usage_report` tinyint(1) NOT NULL DEFAULT 1,
+  `forward_to_secondary` tinyint(1) NOT NULL DEFAULT 1,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `email` (`email`),
+  KEY `domain_id` (`domain_id`),
+  CONSTRAINT `virtual_users_ibfk_1` FOREIGN KEY (`domain_id`) REFERENCES `virtual_domains` (`domain_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `virtual_users`
+--
+
+LOCK TABLES `virtual_users` WRITE;
+/*!40000 ALTER TABLE `virtual_users` DISABLE KEYS */;
+INSERT INTO `virtual_users` VALUES
+(0,0,NULL,'Dummy User for Primary Password Logging',0,0,NULL,'2023-02-22 20:15:50',1,1);
+/*!40000 ALTER TABLE `virtual_users` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'mailserver'
+--
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `resolve_alias` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_unicode_ci */ ;
+DELIMITER ;;
+CREATE DEFINER=`eliyahu`@`localhost` PROCEDURE `resolve_alias`(IN `inmail` VARCHAR(100))
+    READS SQL DATA
+    SQL SECURITY DEFINER
+BEGIN
+set @last_at = - position('@' in reverse(inmail));
+set @first_dash = position('-' in inmail);
+set @length = length(inmail);
+
+IF (@length - (@first_dash - @last_at)) > 0
+	THEN
+		set @identifier = substr(inmail, @first_dash+1, @length - (@first_dash - @last_at));
+		set @user = substring_index(inmail, '-', 1);
+		set @domain = substring_index(inmail, '@', -1);
+
+		SELECT `destination`
+		FROM `virtual_aliases`
+		WHERE
+		(`source`=inmail and `requires_identifier` = 0)
+		or
+		(`requires_identifier` = 1 and @identifier != '' and @identifier REGEXP '[a-z,0-9]+' and `source` = concat(@user, '@', @domain));
+	ELSE
+		SELECT `destination`
+		FROM `virtual_aliases`
+		WHERE
+		(`source`=inmail and `requires_identifier` = 0);
+END IF;
+
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-02-16  9:23:06
